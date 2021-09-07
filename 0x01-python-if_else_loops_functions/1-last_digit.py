f2d1dbb1199bdfbd08394d2257990946f705d519
JessicaJulieth <3497@holbertonschool.com>
2021-09-07 03:26:50 -0500

    Print last digit whit pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 lastD = int(repr(number)[-1])
-of = ("Last digit of {} is {}".format(number, lastD))
 if number < 0:
     lastD *= -1
+of = ("Last digit of {} is {}".format(number, lastD))
 if lastD < 6:
     if lastD != 0:
         print("{} and is less than 6 and not 0".format(of, number, lastD))
 elif lastD > 5:
-    print("{} {} is {} and is greater than 5".format(of, number, lastD))
+    print("{} and is greater than 5".format(of, number, lastD))
 if lastD == 0:
-    print("{} {} is {} and is 0".format(of, number, lastD))
+    print("{} and is 0".format(of, number, lastD))
