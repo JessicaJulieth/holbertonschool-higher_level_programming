508d24eb4c65d1d47ae6f5b564677bb404012e23
JessicaJulieth <3497@holbertonschool.com>
2021-09-08 17:39:48 -0500

    Prints the number of and the list of its arguments the best form

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,8 @@ if __name__ == "__main__":
     if Len == 0:
         print("{} arguments.".format(Len))
     elif Len == 1:
-        print("{} arguments:".format(Len))
+        print("{} argument:".format(Len))
     else:
         print("{} arguments:".format(Len))
-
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
