fcbddacdd2222dfb84a6b329057177a8db14384b
JessicaJulieth <3497@holbertonschool.com>
2021-09-13 15:46:47 -0500

    Retrieve a element from list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx in my_list:
+        return my_list[idx]
+        if idx > 5:
+            return None
+            if idx < 0:
+                return None
