1d5051aa3e16f84971d71af0604ab53cff97aa0e
JessicaJulieth <3497@holbertonschool.com>
2021-09-14 17:24:54 -0500

    Print matrix without []

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    x = 1
+    for idx in matrix:
+        for i in idx:
+            if x < len(idx):
+                print("{}".format(i), end=", ")
+                x += 1
+            else:
+                print("{}".format(i))
+                x = 1
+    if matrix == [[]]:
+        print()
