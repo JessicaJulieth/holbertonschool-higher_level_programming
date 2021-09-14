04548f6a441e4fd9963091ce6c2c5d2c3215134c
JessicaJulieth <3497@holbertonschool.com>
2021-09-14 11:57:39 -0500

    Print without Cc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string[0:]:
+    new_string = ""
+    for i in my_string:
         if i not in ("C", "c"):
-            print("{:s}".format(str(i)), end="")
+            new_string += i
+    return new_string
