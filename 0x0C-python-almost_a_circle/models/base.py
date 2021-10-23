361df7a59991c20dd8698930588c3203110f29d0
JessicaJulieth <3497@holbertonschool.com>
2021-10-22 22:45:28 -0500

    format json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """module that creates a class called Base"""
 
+import json
+
 
 class Base():
     """This class will be the “base” of all other classes in this project.
@@ -19,3 +21,16 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ static method returns the JSON string representation of
+        list_dictionaries
+        Attributes:
+            list_dictionaries: list of dictionaries
+        Returns:
+            json string
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
