460d55b34e5fd5b63a25db7443e90e07fbb57964
JessicaJulieth <3497@holbertonschool.com>
2021-11-17 22:48:49 -0600

    Add table with FOREIGN KEY

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
+    UNIQUE (id),
     state_id INT NOT NULL,
     FOREIGN KEY (state_id) REFERENCES state(id),
     name VARCHAR(256) NOT NULL
