9535f46e1a20b81dbcb5e43e3a2d5a1878b10c0d
JessicaJulieth <3497@holbertonschool.com>
2021-12-13 22:29:44 -0600

    prints a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const myVar = process.argv;
+if (myVar.length < 3){
+    console.log("No argument");
+}
+else if (myVar.length < 4) {
+    console.log("Argument found");
+}
+else {
+    console.log("Arguments found");
+}
