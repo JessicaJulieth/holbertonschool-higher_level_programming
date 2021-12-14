f7bcbfc6bec38752ff7d2bf850068a8774b8d129
JessicaJulieth <3497@holbertonschool.com>
2021-12-14 12:02:32 -0600

    searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,13 @@
 
 const myVar = (process.argv);
 if (myVar.length < 2) {
-  console.log('0')
+  console.log('0');
 } else if (myVar.length <= 3) {
-  console.log('0')
+  console.log('0');
 } else {
-  myVar.sort(function(a,b){
-    return b-a;
+  myVar.sort(function (a, b) {
+    return b - a;
   });
-  
-  console.log(myVar[3]);  
+
+  console.log(myVar[3]);
 }
