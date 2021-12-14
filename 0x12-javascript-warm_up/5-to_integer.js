e63a497e826b331dde86a1f342737c2c1814a83c
JessicaJulieth <3497@holbertonschool.com>
2021-12-14 00:22:13 -0600

    Add task with semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const myVar = (process.argv);
 const x = parseInt(myVar[2]);
-if (myVar[2] === NaN) 
-{
-  console.log("Not a number");
+if (myVar[2] === isNaN) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + x);
 }
-    else 
-    {
-        console.log("My number: " + x);
-    }
