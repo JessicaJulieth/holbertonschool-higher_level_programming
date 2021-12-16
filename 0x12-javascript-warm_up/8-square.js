deb3cd11b0239a5a1364d342bdcb68a82c7b1f09
JessicaJulieth <3497@holbertonschool.com>
2021-12-15 23:30:11 -0600

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const myVar = process.argv[2];
-if (myVar === undefined || myVar == Number) {
+if (myVar === undefined || Number.isNaN(myVar)) {
   console.log('Missing size');
 } else {
   for (let i = 1; i <= myVar; i++) {
