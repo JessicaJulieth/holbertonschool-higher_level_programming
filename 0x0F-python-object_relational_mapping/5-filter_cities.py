306ad208683018a9fbaef818c7a729565e2055c8
JessicaJulieth <3497@holbertonschool.com>
2021-12-19 19:33:26 -0600

    takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,10 +8,17 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities WHERE cities LIKE BINARY\
-                '%{}%' ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT cities.name FROM cities LEFT JOIN states ON\
+                cities.state_id = states.id WHERE states.name = %s\
+                ORDER BY cities.id", (argv[4], ))
+
     query_rows = cur.fetchall()
+    City = []
+    Index = 0
     for row in query_rows:
-        print(row)
+        City.append(query_rows[Index][0])
+        Index += 1
+    Cities = ', '.join(City)
+    print(Cities)
     cur.close()
     db.close()
