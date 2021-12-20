4c7ba9c925f2bc318b9441dc7bee18783dc712e3
JessicaJulieth <3497@holbertonschool.com>
2021-12-19 22:08:11 -0600

    script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,9 +2,9 @@
 """ script that lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
@@ -14,6 +14,6 @@ if __name__ == "__main__":
 
 Session = sessionmaker(bind=engine)
 session = Session()
-for states in session.query(State).order_by(State.id).all():
+for states in session.(State).order_by(State.id).all():
     print("{}: {}".format(states.id, states.name))
 session.close()
