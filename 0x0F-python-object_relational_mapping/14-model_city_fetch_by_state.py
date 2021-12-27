0653cd8e7ec621f6cf47fa31f84ff7e53a4953fa
JessicaJulieth <3497@holbertonschool.com>
2021-12-27 17:04:01 -0600

    prints all City objects from the database

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,16 +4,18 @@
 import sys
 from sqlalchemy import create_engine
 from model_state import Base, State
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from model_state import State, Base
 from model_city import City
+from sqlalchemy import update
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     newt = session.query(State, City).join(City).all()
     for state, city in newt:
         print('{}: ({}) {}'.format(state.name, city.id, city.name))
