99fc1cc536ce5c4187923c50c6fd15f6d673460d
JessicaJulieth <3497@holbertonschool.com>
2022-01-14 00:44:08 -0600

    Display methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sL -X options"$1" | grep "Allow" | cut -d" " -f2-
+curl -sI -X options "$1" | grep "Allow" | cut -d " " -f 2-
