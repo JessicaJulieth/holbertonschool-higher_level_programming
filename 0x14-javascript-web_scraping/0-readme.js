2c15c5564bd980e5c8c04efe5f4228ba92f5798b
JessicaJulieth <3497@holbertonschool.com>
2022-02-01 20:52:02 -0500

    Read and prints the content of a file with semicolon

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf8', function (err, data) {
-  if (err) throw err;
-  console.error(err);
-  console.log(data);
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
