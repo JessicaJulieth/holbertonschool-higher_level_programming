f8ae5b294ef4a1ae0ef884dfbcaaeed05f978b9d
JessicaJulieth <3497@holbertonschool.com>
2022-02-11 12:42:15 -0500

    up task number 2 whit pycode and chmod

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,19 +5,20 @@ sends a POST request to the passed URL with the
 email as a parameter, and displays the body of
 the response (decoded in utf-8)
 """
-import urllib.request, urllib.parse
+import urllib.request
+import urllib.parse
 from sys import argv
 
 
 if __name__ == "__main__":
-	url = argv[1]
-	email = argv[2]
-	value = {'email': email}
+    url = argv[1]
+email = argv[2]
+value = {'email': email}
 
-	data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
+data = urllib.parse.urlencode(value)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
 
-    with urllib.request.urlopen(req) as response:
-        html = response.read().decode('utf-8')
-        print(html)
+with urllib.request.urlopen(req) as response:
+    html = response.read().decode('utf-8')
+    print(html)
