8cfc3ead2774bf36d96148ff7eebfcddde690e1b
JessicaJulieth <3497@holbertonschool.com>
2022-02-15 11:56:18 -0500

    Prints the number of movies with character Wedge Antilles with semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,15 +7,15 @@ request(url, function (err, response, body) {
   if (err) {
     console.error(err);
   } else {
-  const x = JSON.parse(body);
-  let count = 0;
-  for (const y of x.results) {
-    for (const character of y.characters) {
-      if (character.search('/18/') > 0) {
-        count++;
-       }
-     }
-   }
-   console.log(count);
- } 
+    const x = JSON.parse(body);
+    let count = 0;
+    for (const y of x.results) {
+      for (const character of y.characters) {
+        if (character.search('/18/') > 0) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
 });
