2428a260bf380bacb0ca267362caf015e5122092
JessicaJulieth <3497@holbertonschool.com>
2022-02-15 14:35:36 -0500

    Last task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,21 +2,22 @@
 
 const request = require('request');
 const url = process.argv[2];
-const dicc = {};
 
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
+    const dicc = {};
     const data = JSON.parse(body);
     for (const task of data) {
-      if (task.completed && dicc[task.userId]) {
-          dicc[task.userId] += 1;
-        } else if (!dicc[task.userId] && task.completed) {
+      if (task.completed === true) {
+        if (dicc[task.userId] === undefined) {
           dicc[task.userId] = 1;
+        } else {
+          dicc[task.userId] += 1;
         }
       }
     }
-    console.log(dic);
+    console.log(dicc);
   }
 });
