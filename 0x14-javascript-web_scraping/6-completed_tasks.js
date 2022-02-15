bcd1a5c2d98990c48eb66aae58f993ccf24a2955
JessicaJulieth <3497@holbertonschool.com>
2022-02-15 14:04:13 -0500

    computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,7 +4,7 @@ const request = require('request');
 const url = process.argv[2];
 const dicc = {};
 
-request(url, function (error, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
